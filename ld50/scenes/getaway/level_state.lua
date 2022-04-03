7cfbd5ffec789d49a47f767cc6a87528a725d1b0
Artsiom Trubchyk <aglitchman@gmail.com>
2022-04-03 20:32:13 +0300

    Add score counting

--- ld50/scenes/getaway/level_state.lua
+++ ld50/scenes/getaway/level_state.lua
@@ -3,6 +3,11 @@ local M = {}
 function M.reset()
     M.car_speed = 0
     M.car_count = 0
+    M.car_distance = 0
+end
+
+function M.calc_score()
+    return math.floor(M.car_distance / 30)
 end
 
 M.reset()
