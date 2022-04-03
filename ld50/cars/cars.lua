d4f81e1966c08ea63ddde9363ae79986ca247457
Artsiom Trubchyk <aglitchman@gmail.com>
2022-04-03 11:17:19 +0300

    Put car control into car_main.script

--- ld50/cars/cars.lua
+++ ld50/cars/cars.lua
@@ -4,7 +4,6 @@ function M.spawn(collectionfactory_url, position, rotation)
     local car = collectionfactory.create(collectionfactory_url, position, rotation, nil)
 
     local car_main = msg.url(nil, car[hash("/main")], "car_main")
-    local car_control = msg.url(nil, car[hash("/main")], "car_control")
 
     go.set(car_main, "car_physics_id",        car[hash("/physics")])
     go.set(car_main, "car_physics_wheel1_id", car[hash("/physics_wheel_fl")])
@@ -18,10 +17,7 @@ function M.spawn(collectionfactory_url, position, rotation)
     go.set(car_main, "car_visuals_wheel3_id", car[hash("/visuals_wheel_rl")])
     go.set(car_main, "car_visuals_wheel4_id", car[hash("/visuals_wheel_rr")])
 
-    go.set(car_control, "my_car_obj_url",       msg.url(nil, car[hash("/visuals")], nil))
-    go.set(car_control, "my_car_collision_url", msg.url(nil, car[hash("/physics")], hash("collision")))
-
-    return car_main, car_control, car[hash("/visuals")]
+    return car_main, car[hash("/visuals")]
 end
 
 return M
