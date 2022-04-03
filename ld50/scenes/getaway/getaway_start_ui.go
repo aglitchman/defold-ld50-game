8d56360cddb3a97a7c3f7f4adb4f16865110d5db
Artsiom Trubchyk <aglitchman@gmail.com>
2022-04-03 20:54:01 +0300

    Add start UI

---
+++ ld50/scenes/getaway/getaway_start_ui.go
@@ -0,0 +1,30 @@
+components {
+  id: "gui"
+  component: "/ld50/scenes/getaway/getaway_start_ui.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "delete_go"
+  component: "/ld50/scripts/delete_go.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
