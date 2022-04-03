de24f9703e8f06b986e1ca4ea62c7c052be1970e
Artsiom Trubchyk <aglitchman@gmail.com>
2022-04-04 02:14:38 +0300

    Add ground (sand)

---
+++ ld50/assets/meshes/ground_sand.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.bounds_min = vmath.vector3(-50.0, -0.10000000149, -10.0)
+M.bounds_max = vmath.vector3(50.0, 0, 10.0)
+M.size = vmath.vector3(100.0, 0.10000000149, 20.0)
+
+return M
---
+++ ld50/road/ground_sand.go
@@ -0,0 +1,70 @@
+embedded_components {
+  id: "mesh"
+  type: "mesh"
+  data: "material: \"/scene3d/materials/basic_color.material\"\n"
+  "vertices: \"/ld50/assets/meshes/ground_sand.buffer\"\n"
+  "textures: \"/scene3d/assets/textures/grid_20x20.png\"\n"
+  "primitive_type: PRIMITIVE_TRIANGLES\n"
+  "position_stream: \"position\"\n"
+  "normal_stream: \"normal\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_STATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.5\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: -0.1\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 50.0\n"
+  "  data: 0.1\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
