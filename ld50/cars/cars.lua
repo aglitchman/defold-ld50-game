66d04c0b89b9540db9b8f8f5ef254928a3969a4c
Artsiom Trubchyk <aglitchman@gmail.com>
2022-04-03 15:32:01 +0300

    Add merging for real cars

--- ld50/cars/cars.lua
+++ ld50/cars/cars.lua
@@ -1,9 +1,48 @@
 local M = {}
 
+M.list = {}
+
+function M.register(physics_id, car_main, car_collection)
+    table.insert(M.list, {
+        physics_id = physics_id,
+        car_main = car_main,
+        collection = car_collection
+    })
+end
+
+function M.find_by_physics(physics_id)
+    for i, v in ipairs(M.list) do
+        if v.physics_id == physics_id then
+            return v
+        end
+    end
+
+    return nil
+end
+
+function M.unregister(physics_id)
+    for i, v in ipairs(M.list) do
+        if v.physics_id == physics_id then
+            table.remove(M.list, i)
+            break
+        end
+    end
+end
+
+function M.delete(physics_id)
+    for i, v in ipairs(M.list) do
+        if v.physics_id == physics_id then
+            go.delete(v.collection)
+            break
+        end
+    end
+end
+
 function M.spawn(collectionfactory_url, position, rotation)
     local car = collectionfactory.create(collectionfactory_url, position, rotation, nil)
 
     local car_main = msg.url(nil, car[hash("/main")], "car_main")
+    local car_merge = msg.url(nil, car[hash("/physics")], "car_merge")
 
     go.set(car_main, "car_physics_id",        car[hash("/physics")])
     go.set(car_main, "car_physics_wheel1_id", car[hash("/physics_wheel_fl")])
@@ -16,6 +55,10 @@ function M.spawn(collectionfactory_url, position, rotation)
     go.set(car_main, "car_visuals_wheel2_id", car[hash("/visuals_wheel_fr")])
     go.set(car_main, "car_visuals_wheel3_id", car[hash("/visuals_wheel_rl")])
     go.set(car_main, "car_visuals_wheel4_id", car[hash("/visuals_wheel_rr")])
+
+    go.set(car_merge, "car_main_script", car_main)
+
+    M.register(car[hash("/physics")], car_main, car)
 
     return car_main, car[hash("/visuals")]
 end
--- ld50/fx/scripts/fx.lua
+++ ld50/fx/scripts/fx.lua
@@ -2,11 +2,20 @@ local M = {}
 
 -- TODO
 local FX_DUST_URL = "/fx#fx_dust"
+local ROAD_DUST = hash("road_dust")
+local PUFF = hash("puff")
 
 function M.road_dust(position, scale)
-    msg.post(FX_DUST_URL, "road_dust", {
+    msg.post(FX_DUST_URL, ROAD_DUST, {
         position = position,
         scale = scale
+    })
+end
+
+function M.puff(position, parent_id)
+    msg.post(FX_DUST_URL, PUFF, {
+        position = position,
+        parent_id = parent_id
     })
 end
 
