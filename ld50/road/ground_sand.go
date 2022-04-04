2819d32b6ee471da2f1b6f3666f90e902a190d72
Artsiom Trubchyk <aglitchman@gmail.com>
2022-04-04 03:06:28 +0300

    Add autoslowdown + speedometer

--- ld50/road/ground_sand.go
+++ ld50/road/ground_sand.go
@@ -21,7 +21,7 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "collisionobject"
+  id: "collisionobject_bottom"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
   "type: COLLISION_OBJECT_TYPE_STATIC\n"
@@ -49,6 +49,102 @@ embedded_components {
   "  }\n"
   "  data: 50.0\n"
   "  data: 0.1\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject_left"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_STATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: -50.0\n"
+  "      y: 1.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 0.5\n"
+  "  data: 1.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject_right"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_STATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 50.0\n"
+  "      y: 1.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 0.5\n"
+  "  data: 1.0\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
--- ld50/scenes/getaway/level_state.lua
+++ ld50/scenes/getaway/level_state.lua
@@ -1,10 +1,16 @@
 local M = {}
 
 function M.reset()
+    -- UI
     M.car_speed = 0
-    M.car_boost = 0
-    M.car_count = 0
+    M.car_accel_boost = 0
+    M.car_accel_const = 0
+
+    -- Score
     M.car_distance = 0
+
+    -- Debug
+    M.car_count = 0
 end
 
 function M.calc_score()
